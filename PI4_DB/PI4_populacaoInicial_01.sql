3ebfc947c001b243afff4123542be051cbca9fb2
Deive <deive.leal@gmail.com>
2020-11-30 16:24:05 -0300

    Adicionado população Aluno e Avalização Física

--- PI4_DB/PI4_populacaoInicial_01.sql
+++ PI4_DB/PI4_populacaoInicial_01.sql
@@ -13,18 +13,34 @@ VALUES
 select * from ListaExercicios;
 
 INSERT INTO Aluno(
-	nome, dataNascimento, sexo,
-    cpf, email, tipoSanguineo,
-    rhSangue, telefone, celular,
-    rua, bairro, cidade)
+	nome, dataNascimento, sexo, cpf, 
+    email, tipoSanguineo, rhSangue, 
+    telefone, celular, rua, 
+    bairro, cidade)
 VALUES
-	('João Carlos', '25/03/2000', 'masulino', '03265498724','joao@jotaum.com', 'A', '+', '1932567412', '19852321654', 'Rua das Flores, 321', 'Jardim Novo', 'Joalandia'),
-    ('Maria de Souza', '12/12/1991', 'feminino', '02165498752', 'maria@sozinha.com', 'B', '-', '2121256523', '21789456123', 'Rua Litu, 12', 'Jardim Velhor', 'Joalandia'),
-    ('Jessi de Oliveira', '21/07/2002', 'nao-binario', '04532198758', 'jessi@jotaum.com', 'AB', '2121257889', '21852741963', 'Rua Janguá, 25', 'Jardim Esplendor', 'Joanopolis'),
-    ('Paula Campos', '13/12/2001', 'feminino', '08945612378', 'paulinha@jotaum.com', 'O', '2134569874', '21854978321', 'Rua Joá, 451', 'Jardim Novo', 'Joalandia'),
-    ('Denis Drato', '05/01/1999', 'masculino', '06952456845', 'ddrato@sozinha.com', 'O', '2134569596', '21985654321', 'Rua das Uvas, 69', 'Jardim Novo Sonho', 'Jardim Esplendor', 'Joanopolis');
+	('João Carlos', '2000/03/25', 'masculino', '03265498724','joao@jotaum.com', 'A', '+', '1932567412', '19852321654', 'Rua das Flores, 321', 'Jardim Novo', 'Joalandia'),
+    ('Maria de Souza', '1991/12/12', 'feminino', '02165498752', 'maria@solzinha.com', 'B', '-', '2121256523', '21789456123', 'Rua Litu, 12', 'Jardim Velhor', 'Joalandia'),
+    ('Jessi de Oliveira', '2002/07/21', 'nao-buinario', '04532198758', 'jessi@jotaum.com', 'AB', '-', '2121257889', '21852741963', 'Rua Janguá, 25', 'Jardim Esplendor', 'Joanopolis'),
+    ('Paula Campos', '2001/12/13', 'feminino', '08945612378', 'paulinha@jotaum.com', 'O', '-', '2134569874', '21854978321', 'Rua Joá, 451', 'Jardim Novo', 'Joalandia'),
+    ('Denis Drato', '1999/01/05', 'masculino', '06952456845', 'ddrato@solzinha.com', 'O', '+', '2134569596', '21985654321', 'Rua das Uvas, 69', 'Jardim Novo Sonho', 'Joanopolis');
     
+select * from Aluno;
+
+INSERT INTO AvaliacaoFisica(
+	idAluno, dataAvaliacao, peso,
+    altura, pressaoArt, batimentoRepouso,
+    nivelCondicionamento
+)
+VALUES
+	(21,'2020/01/23', 83.0, 1.60, 13.9, 82, 'iniciante'),
+    (22,'2020/01/22', 82.0, 1.80, 12.8, 65, 'intermediario'),
+    (23,'2020/01/19', 72.0, 1.70, 12.8, 68, 'iniciante'),
+    (24,'2020/01/24', 93.0, 1.80, 12.8, 60, 'avançado'),
+    (25,'2020/01/25', 55.0, 1.53, 12.8, 58, 'iniciante'),
+    (22,'2020/04/22', 82.4, 1.80, 12.8, 65,'intermediario'),
+    (24,'2020/04/24', 93.6, 1.80, 12.8, 60, 'avançado');
     
+select * from AvaliacaoFisica;
     
     
     
