6750bde57b55767c5c39026593f796ead6b691e7
fusalina <zanotto.carol@gmail.com>
2020-12-10 18:21:02 -0300

    alteraçoes na classe plano do model

--- PI4_Code/Efeito/src/Model/Plano.java
+++ PI4_Code/Efeito/src/Model/Plano.java
@@ -11,10 +11,13 @@ import java.util.Date;
  *
  * @author deive
  */
-public class Plano extends RegistroAlunos {
+public class Plano{
     private double valor;
     private Date dataInicio;
     private Date dataFinal;
+    private int numeroAulasPorSemana;
+    private int numeroAulasPorMes;
+    private int numeroAulasTotalDoPlano;
     
     //Métodos acessores
 
@@ -41,6 +44,39 @@ public class Plano extends RegistroAlunos {
     public void setDataFinal(Date dataFinal) {
         this.dataFinal = dataFinal;
     }
+
+    public int getNumeroAulasPorSemana() {
+        return numeroAulasPorSemana;
+    }
+
+    public void setNumeroAulasPorSemana(int numeroAulasPorSemana) {
+        this.numeroAulasPorSemana = numeroAulasPorSemana;
+    }
+
+    public int getNumeroAulasPorMes() {
+        return numeroAulasPorMes;
+    }
     
+    public void setNumeroAulasPorMes() {
+        //metodo para calcular numero de aulas por mes(em relação ao mes)
+        this.numeroAulasPorMes = this.numeroAulasPorSemana * 4;
+    }
+    
+    public void setNumeroAulasPorMes(int numeroAulasPorMes) {
+        this.numeroAulasPorMes = numeroAulasPorMes;
+    }
+
+    public int getNumeroAulasTotalDoPlano() {
+        return numeroAulasTotalDoPlano;
+    }
+
+    public void setNumeroAulasTotalDoPlano(int numeroAulasTotalDoPlano) {
+        this.numeroAulasTotalDoPlano = numeroAulasTotalDoPlano;
+    }
+    
+    public void setNumeroAulasTotalDoPlano() {
+        // alterar valor 3 para funcão que calcula numero de meses do plano
+        this.numeroAulasTotalDoPlano = this.numeroAulasPorMes * 3;
+    }
     
 }
