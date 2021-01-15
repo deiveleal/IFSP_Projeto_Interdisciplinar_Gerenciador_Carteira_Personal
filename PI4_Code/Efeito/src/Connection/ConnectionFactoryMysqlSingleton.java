2dbc3c57c3079b76c2c33e5ebedfe70d8a2338b5
Deive <deive.leal@gmail.com>
2021-01-15 16:47:28 -0300

    Tentativa de implementação de singleton para conexão com o banco

---
+++ PI4_Code/Efeito/src/Connection/ConnectionFactoryMysqlSingleton.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Carolina
+ * @author Deive
+ *
+ */
+
+public class ConnectionFactoryMysqlSingleton implements ConnectionFactoryInterface{
+    private final String nomeBanco = "sistema_efeito";//Nome do banco de dados
+    private final String nomeUsuario = "root";//Usuário do servidor
+    private final String senhaUsuario = "123456";//Senha do servidor
+    private final String enderecoServidor = "localhost";
+    private static ConnectionFactoryMysqlSingleton conexao;
+
+
+    private ConnectionFactoryMysqlSingleton(){
+        
+    }
+
+    public ConnectionFactoryMysqlSingleton connection(){
+        if(conexao == null){
+            ConnectionFactoryMysqlSingleton.conexao = new ConnectionFactoryMysqlSingleton();
+        }
+        return conexao;
+    }
+            
+    @Override
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://" + enderecoServidor + "/" + nomeBanco,
+                    nomeUsuario,
+                    senhaUsuario);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Connection closeConnection() {
+        try {
+            getConnection().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactoryMysql.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
