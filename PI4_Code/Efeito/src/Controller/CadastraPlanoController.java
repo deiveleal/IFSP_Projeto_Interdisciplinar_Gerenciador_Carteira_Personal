f71f5a220a008d048fa39b120a6b0b4a35478c2b
fusalina <zanotto.carol@gmail.com>
2021-01-25 12:53:20 -0300

    biblioteca jar + cadastroplanoController

--- PI4_Code/Efeito/src/Controller/CadastraPlanoController.java
+++ PI4_Code/Efeito/src/Controller/CadastraPlanoController.java
@@ -1,103 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controller;
-
-import Model.ModelScreen.CadastraAlunoScr;
-import Model.ModelScreen.CadastraPlanoScr;
-import Model.ModelScreen.MenuPrincipalScr;
-import Model.Util;
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-/**
- * FXML Controller class
- *
- * @author carolina
- */
-public class CadastraPlanoController implements Initializable {
-    
-    @FXML    private Text t_titulo;
-    @FXML    private TextField tf_nome_aluno;
-    @FXML    private TextField tf_id_aluno;
-    @FXML    private Text t_titulo1;
-    @FXML    private Text t_titulo11;
-    @FXML    private TextField tf_valor;
-    @FXML    private Button bt_menu;
-    @FXML    private Button bt_sair;
-    @FXML    private Button bt_salvar;
-    
-    Util util = new Util();
- 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    // Implementações do botão menu iniciar
-    public void initialize(URL url, ResourceBundle rb) {
-        
-        //Implementações do botão menu inicial
-        bt_menu.setOnMouseClicked((MouseEvent e) -> {
-            MenuPrincipalScr menuPrinc = new MenuPrincipalScr();
-            try {
-                menuPrinc.start(new Stage());
-                fechaJanela();
-            }
-            catch (Exception ex) {
-                Logger.getLogger(CadastraPlanoController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-        bt_menu.setOnKeyPressed((KeyEvent e) -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                MenuPrincipalScr menuPrinc = new MenuPrincipalScr();
-
-                try {
-                    menuPrinc.start(new Stage());
-                    fechaJanela();
-                }
-                catch (Exception ex) {
-                    Logger.getLogger(CadastraPlanoController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        
-        //Implementações do botão Sair
-        bt_sair.setOnMouseClicked((MouseEvent e) -> {
-            util.voltaTelaLogin();
-            fechaJanela();
-        });   
-        bt_sair.setOnKeyPressed((KeyEvent e) -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                util.voltaTelaLogin();
-                fechaJanela();
-            }
-        });
-    
-    }    
-    private void fechaJanela() {
-        CadastraPlanoScr.getStage().close();
-        }
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Model.ModelComboBox.QuantidadeMesesCB;
@@ -106,9 +6,13 @@ import Model.ModelScreen.CadastraPlanoScr;
 import Model.ModelScreen.MenuPrincipalScr;
 import Model.Util;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -136,6 +40,8 @@ public class CadastraPlanoController implements Initializable {
     @FXML    private ComboBox<QuantidadeMesesCB> cb_tipoplano;
     
     private List<QuantidadeMesesCB> quantidadeMeses = new ArrayList<>();
+    private ObservableList<QuantidadeMesesCB> obsQuantidadeMesesCB;
+
     
     Util util = new Util();
  
@@ -202,7 +108,9 @@ public class CadastraPlanoController implements Initializable {
         quantidadeMeses.add(qtdMeses04);
         quantidadeMeses.add(qtdMeses05);
         
-        cb_tipoplano.setItems(quantidadeMeses);
+        obsQuantidadeMesesCB = FXCollections.observableArrayList(quantidadeMeses);
+        
+        cb_tipoplano.setItems(obsQuantidadeMesesCB);
     }
     
     private void fechaJanela() {
@@ -213,4 +121,4 @@ public class CadastraPlanoController implements Initializable {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
->>>>>>> 62e870fb416a975a5314aa12780a7495b0dfb00a
+
